7591989472283daaa31757aa5519adda4f116c1d
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-03-14 19:43:28 +0330

    feat: header Componet

---
+++ componets/Hedaer.js
@@ -0,0 +1,34 @@
+import { Logo } from '../statics/svgs'
+
+const Header = () => (
+  <Wrapper>
+    <TitleCountainer>
+      <LogoCountainer>
+        <Logo />
+      </LogoCountainer>
+      <Title>CrypTubers</Title>
+    </TitleCountainer>
+  </Wrapper>
+)
+
+export default Header
+
+const Wrapper = styled.h1`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+`
+const TitleCountainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+`
+const Title = styled.h1`
+  font-family: 'roboto';
+  font-size: 24px;
+`
+const LogoCountainer = styled.div`
+  width: 44px;
+  height: 34px;
+`

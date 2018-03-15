cc5582e5ceee6a96fcd3ae2f8774726b94f587f0
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-03-15 14:40:39 +0330

    feat: add Navbar

--- componets/Hedaer.js
+++ componets/Hedaer.js
@@ -10,6 +10,10 @@ const Header = () => (
       </LogoCountainer>
       <Title>CrypTubers</Title>
     </TitleCountainer>
+    <NavCountainer>
+      <NavItem href="#">About</NavItem>
+      <NavItem href="#">New Channel</NavItem>
+    </NavCountainer>
   </Wrapper>
 )
 
@@ -17,20 +21,46 @@ export default Header
 
 const Wrapper = styled.h1`
   width: 100%;
-  height: 60px;
+  height: 40px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding-top: 20px;
 `
 const TitleCountainer = styled.div`
   height: 100%;
   display: flex;
   justify-content: flex-start;
+  align-items: center;
 `
 const Title = styled.h1`
   font-family: 'roboto';
   font-size: 24px;
+  margin-left: 5px;
+  cursor: pointer;
 `
 const LogoCountainer = styled.div`
   width: 44px;
-  height: 34px;
+  height: 44px;
+  transform: rotate(0deg);
+  transition: all 200ms ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    transform: rotate(-20deg);
+  }
+`
+
+const NavCountainer = styled.nav`
+  width: 150px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const NavItem = styled.a`
+  font-family: 'roboto';
+  font-size: 16px;
+  font-weight: 400;
+  color: #000;
+  text-decoration: none;
 `
